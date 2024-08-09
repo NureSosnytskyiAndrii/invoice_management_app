d28ab02ab0b010ccbfce676f41b07aef0dcd0dd3
annndre <andr.sosn@gmail.com>
2024-08-09 22:03:16 +0300

    init project

---
+++ src/components/WelcomeComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <button @click="navigateToInputPage" class="btn btn-primary">Start Calculating</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navigateToInputPage() {
+      this.$router.push({ name: 'InputPage' });
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+button {
+  padding: 10px 20px;
+  font-size: 18px;
+}
+</style>
