ced5fb0a4d085e05c32a308a1fe30d4ed0c174d6
annndre <andr.sosn@gmail.com>
2024-08-13 12:55:23 +0300

    adding dist folder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
+// vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/invoice_management_app/'
-      : '/',
-}
+  publicPath: '/invoice_management_app/'
+};
+
